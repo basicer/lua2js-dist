5003adac046da6a752c702ae727fe49f48dd1a4e
basicer <basicer@basicer.com>
2014-05-13 02:40:23 -0400

    Snapshot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 lua2js-dist
 ===========
+
+Because I hate having my built stuff in the same repo.
---
+++ index.js
@@ -0,0 +1,8 @@
+
+var parser = require('./parser.js');
+var stdlib = require('./stdlib.js');
+
+this.stdlib = stdlib;
+this.parse = parser.parser.parse;
+
+
